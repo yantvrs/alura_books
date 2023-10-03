7f568b0bf71948b8a89efc9c97cd0775a43b9e40
Yan Tavares <yantavares2002@gmail.com>
2023-10-03 16:00:34 -0300

    Organizando header de forma centralizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Logo from './componentes/Logo'
+import Perfil from './imagens/perfil.svg'
+import Sacola from './imagens/sacola.svg'
+
+const textOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icones = [Perfil, Sacola]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='opcoes'>
+          { textOpcoes.map( (texto) => (
+            <li className='opcao'><p>{texto}</p></li>
+          ))}
+        </ul>
+
+        <ul className='icones-header'>
+            { icones.map( (icone) => (<li className='icone'><img src={icone}></img></li>
+            ))}
+        </ul>
       </header>
     </div>
   );
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,7 +4,11 @@ import logo from '../../imagens/logo.svg'
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='logo'></img>
+            <img 
+            src={logo} 
+            alt='logo' 
+            className='logo-image'
+            ></img>
             <p><strong>Alura</strong>Books</p>
         </div>
     )
