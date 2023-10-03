71c61fe46b1aaa2dcc8e3ee91278e8db630a0dff
Yan Tavares <yantavares2002@gmail.com>
2023-10-03 15:22:20 -0300

    Componentização header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import logo from './imagens/logo.svg'
+import Logo from './componentes/Logo'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>AluraBooks</strong></p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
